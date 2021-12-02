52aaf9a3aac3af9ed41c1d5e5f92d6f9728bdf24
camille <camille.lavarde@edu.devinci.fr>
2021-12-01 19:54:09 -0500

    basic reso

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace ShapeFight
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        public Transform camera;
+
+        private void Awake()
+        {
+            if (!instance)
+                instance = this;
+            else
+                Destroy(this);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+
+                SubmitNewPosition();
+            }
+
+            GUILayout.EndArea();
+        }
+
+        static void StartButtons()
+        {
+            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+        }
+        static void StatusLabels()
+        {
+            var mode = NetworkManager.Singleton.IsHost ?
+                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+            GUILayout.Label("Transport: " +
+                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+            GUILayout.Label("Mode: " + mode);
+        }
+
+        static void SubmitNewPosition()
+        {
+            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
+            {
+                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+                var player = playerObject.GetComponent<Player>();
+                player.Move();
+            }
+        }
+    }
+}
